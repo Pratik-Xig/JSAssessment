5aaeea106ccceef2bcdfd3ea0e974e4a00b40000
Pratik Mishra <116499548+Pratik-Xig@users.noreply.github.com>
2024-06-02 02:53:32 +0530

    Update JSproj.js

--- JSproj.js
+++ JSproj.js
@@ -1,37 +1,44 @@
 const NFTinfo=[];
+let count=0;
 
 
-function mintNFT (n,wi,ti)
+
+function mintNFT (n,wi,ti,dob)
 {
   const o={};
   o.Name=n
+  o.DOB=dob
   o.Work_Id=wi
   o.Transaction_Id=ti
+  count++;
   NFTinfo.push(o)
 }
 
-
-
 function listnfts()
 {
-for(let i=0;i<NFTinfo.length;i++)
+for(let i=0; i < NFTinfo.length ; i++)
   {
-    console.log("\n"+"ID: "+ (i+1) );
-    console.log(NFTinfo[i]);
+    console.log("\n"+"NFT with ID: "+ NFTinfo[i].Work_Id);
+    console.log("---------------------------------------")
+    console.log("Name: " + NFTinfo[i].Name );
+    console.log("Date Of Birth: " + NFTinfo[i].DOB);
+    console.log("Transaction ID: " + NFTinfo[i].Transaction_Id);
+   
+
   }
 }
 
-
-
 function getTotalSupply()
 {
-  console.log("\nTotal NFT's Stroed in Database: "+ NFTinfo.length)
+  console.log("\nTotal NFT's Stored in Database: "+ count)
 }
 
 
 
-mintNFT("Pratik","69877","IO-JK9076")
-mintNFT("Anshit","89439","PL=9OKH9K")
+mintNFT("Pratik","1","IO-JK9076","29-03-03")
+mintNFT("Anshit","2","PL=9OKH9K","89-89-89")
+
 
 getTotalSupply();
+
 listnfts();
